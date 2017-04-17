1d1e773ce83900c0e7ac62a58d5984aa3497a8db
Elianny Arias <arias.vzla@gmail.com>
2017-04-17 15:10:23 +0100

    create java class

---
+++ src/main/java/com/mycompany/banking/system/accounts.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.banking.system;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Elian Arias
+ */
+public class accounts implements 
+Serializable
+ {
+    private int accountid;
+    private String accountnumber;
+    private double balance;
+    private int sortcode;
+    
+}
