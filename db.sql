49466e4d224fbd3d5efd395fa325c88331d4d234
XIN LYU <lyu8869@gmail.com>
2017-04-18 15:20:40 +0100

    database

--- db.sql
+++ db.sql
@@ -1,144 +1,104 @@
-CREATE DATABASE  IF NOT EXISTS `bankingsys2` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `bankingsys2`;
--- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
---
--- Host: localhost    Database: bankingsys2
--- ------------------------------------------------------
--- Server version	5.7.16-log
+-- MySQL Workbench Forward Engineering
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
---
--- Table structure for table `accounts`
---
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema bankingsys2
+-- -----------------------------------------------------
 
-DROP TABLE IF EXISTS `accounts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `accounts` (
-  `accountid` int(11) NOT NULL,
-  `accountnumber` varchar(20) DEFAULT NULL,
-  `balance` decimal(10,0) DEFAULT NULL,
-  `sortcode` int(11) DEFAULT NULL,
-  `customer_custid` int(11) NOT NULL,
-  `branch_branchid` int(11) NOT NULL,
-  PRIMARY KEY (`accountid`,`customer_custid`,`branch_branchid`),
-  KEY `fk_accounts_customer1_idx` (`customer_custid`),
-  KEY `fk_accounts_branch1_idx` (`branch_branchid`),
-  CONSTRAINT `fk_accounts_branch1` FOREIGN KEY (`branch_branchid`) REFERENCES `branch` (`branchid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_accounts_customer1` FOREIGN KEY (`customer_custid`) REFERENCES `customer` (`custid`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- -----------------------------------------------------
+-- Schema bankingsys2
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `bankingsys2` DEFAULT CHARACTER SET utf8 ;
+USE `bankingsys2` ;
 
---
--- Dumping data for table `accounts`
---
+-- -----------------------------------------------------
+-- Table `bankingsys2`.`customer`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bankingsys2`.`customer` (
+  `custid` INT(11) NOT NULL,
+  `firstname` VARCHAR(20) NULL DEFAULT NULL,
+  `lastname` VARCHAR(20) NULL DEFAULT NULL,
+  `address` VARCHAR(20) NULL DEFAULT NULL,
+  `email` VARCHAR(20) NULL DEFAULT NULL,
+  `password` VARCHAR(10) NULL DEFAULT NULL,
+  PRIMARY KEY (`custid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-LOCK TABLES `accounts` WRITE;
-/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `branch`
---
+-- -----------------------------------------------------
+-- Table `bankingsys2`.`branch`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bankingsys2`.`branch` (
+  `branchid` INT(11) NOT NULL,
+  `phonenumber` VARCHAR(20) NULL DEFAULT NULL,
+  `accounts_accountid` INT(11) NOT NULL,
+  PRIMARY KEY (`branchid`),
+  INDEX `fk_branch_accounts1_idx` (`accounts_accountid` ASC),
+  CONSTRAINT `fk_branch_accounts1`
+    FOREIGN KEY (`accounts_accountid`)
+    REFERENCES `bankingsys2`.`accounts` (`accountid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-DROP TABLE IF EXISTS `branch`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `branch` (
-  `branchid` int(11) NOT NULL,
-  `phonenumber` varchar(20) DEFAULT NULL,
-  `accounts_accountid` int(11) NOT NULL,
-  `accounts_customer_custid` int(11) NOT NULL,
-  PRIMARY KEY (`branchid`,`accounts_accountid`,`accounts_customer_custid`),
-  KEY `fk_branch_accounts1_idx` (`accounts_accountid`,`accounts_customer_custid`),
-  CONSTRAINT `fk_branch_accounts1` FOREIGN KEY (`accounts_accountid`, `accounts_customer_custid`) REFERENCES `accounts` (`accountid`, `customer_custid`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `branch`
---
+-- -----------------------------------------------------
+-- Table `bankingsys2`.`accounts`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bankingsys2`.`accounts` (
+  `accountid` INT(11) NOT NULL,
+  `accountnumber` VARCHAR(20) NULL DEFAULT NULL,
+  `balance` DECIMAL(10,0) NULL DEFAULT NULL,
+  `sortcode` INT(11) NULL DEFAULT NULL,
+  `customer_custid` INT(11) NULL,
+  `customer_custid1` INT(11) NOT NULL,
+  `branch_branchid` INT(11) NOT NULL,
+  PRIMARY KEY (`accountid`),
+  INDEX `fk_accounts_customer_idx` (`customer_custid1` ASC),
+  INDEX `fk_accounts_branch1_idx` (`branch_branchid` ASC),
+  CONSTRAINT `fk_accounts_customer`
+    FOREIGN KEY (`customer_custid1`)
+    REFERENCES `bankingsys2`.`customer` (`custid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_accounts_branch1`
+    FOREIGN KEY (`branch_branchid`)
+    REFERENCES `bankingsys2`.`branch` (`branchid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-LOCK TABLES `branch` WRITE;
-/*!40000 ALTER TABLE `branch` DISABLE KEYS */;
-/*!40000 ALTER TABLE `branch` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `customer`
---
+-- -----------------------------------------------------
+-- Table `bankingsys2`.`transactions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bankingsys2`.`transactions` (
+  `trid` INT(11) NOT NULL,
+  `phonenumber` VARCHAR(20) NULL DEFAULT NULL,
+  `typeoft` VARCHAR(20) NULL DEFAULT NULL,
+  `description` VARCHAR(20) NULL DEFAULT NULL,
+  `date` DATE NULL DEFAULT NULL,
+  `accounts_accountid` INT(11) NOT NULL,
+  PRIMARY KEY (`trid`),
+  INDEX `fk_transactions_accounts1_idx` (`accounts_accountid` ASC),
+  CONSTRAINT `fk_transactions_accounts1`
+    FOREIGN KEY (`accounts_accountid`)
+    REFERENCES `bankingsys2`.`accounts` (`accountid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-DROP TABLE IF EXISTS `customer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `customer` (
-  `custid` int(11) NOT NULL,
-  `firstname` varchar(20) DEFAULT NULL,
-  `lastname` varchar(20) DEFAULT NULL,
-  `address` varchar(20) DEFAULT NULL,
-  `email` varchar(20) DEFAULT NULL,
-  `password` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`custid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `customer`
---
-
-LOCK TABLES `customer` WRITE;
-/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `transactions`
---
-
-DROP TABLE IF EXISTS `transactions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `transactions` (
-  `trid` int(11) NOT NULL,
-  `phonenumber` varchar(20) DEFAULT NULL,
-  `typeoft` varchar(20) DEFAULT NULL,
-  `description` varchar(20) DEFAULT NULL,
-  `date` date DEFAULT NULL,
-  `accounts_accountid` int(11) NOT NULL,
-  `accounts_customer_custid` int(11) NOT NULL,
-  PRIMARY KEY (`trid`,`accounts_accountid`,`accounts_customer_custid`),
-  KEY `fk_transactions_accounts1_idx` (`accounts_accountid`,`accounts_customer_custid`),
-  CONSTRAINT `fk_transactions_accounts1` FOREIGN KEY (`accounts_accountid`, `accounts_customer_custid`) REFERENCES `accounts` (`accountid`, `customer_custid`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `transactions`
---
-
-LOCK TABLES `transactions` WRITE;
-/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-04-17 15:35:34
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
